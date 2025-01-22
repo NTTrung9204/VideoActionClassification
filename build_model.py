64d152d92c3776d58ff7b65cd539c2ae5ea190ff
Trung Nguyễn <trung9204@gmail.com>
2025-01-22 22:34:00 +0700

    feat: build model LSTM

---
+++ build_model.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+
+class LSTMModel(nn.Module):
+    def __init__(self, hidden_dim, input_dim, num_layers, num_classes):
+        super(LSTMModel, self).__init__()
+
+        self.hidden_dim = hidden_dim
+        self.input_dim = input_dim
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+
+        self.fcs = nn.Linear(hidden_dim, num_classes)
+
+    def forward(self, x):
+        x, _ = self.lstm(x)
+
+        x = self.fcs(x[:, -1, :])
+
+        return x
