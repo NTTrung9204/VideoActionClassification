1f5e132a9e1b2b78be61632cf5d5e42eca84f5e9
Trung Nguyễn <trung9204@gmail.com>
2025-01-23 13:29:32 +0700

    modify: get data directly from encoding data

--- UCFDataset.py
+++ UCFDataset.py
@@ -1,17 +1,18 @@
 from torch.utils.data import Dataset
-from utils import extract_feature_video
 
 class UCFDataset(Dataset):
-    def __init__(self, data, folder_path):
+    def __init__(self, data, mapping, list_video_name):
         super().__init__()
 
         self.data = data
-        self.folder_path = folder_path
+        self.mapping = mapping
+        self.list_video_name = list_video_name
 
     def __len__(self):
-        return len(self.data)
+        return len(self.list_video_name)
     
     def __getitem__(self, index):
-        video_name, label = self.data[index].split()
-        return extract_feature_video(self.folder_path + video_name), label
+        video_name = self.list_video_name[index]
+        action_name, _ = video_name.split("/")
+        return self.data[video_name], self.mapping[action_name]
     
