dc8acb7abe79674d855cddd1e57387e32de1704c
Trung Nguyễn <trung9204@gmail.com>
2025-02-02 16:21:29 +0700

    feat: create simple RNN model

--- build_model.py
+++ build_model.py
@@ -18,3 +18,22 @@ class LSTMModel(nn.Module):
         x = self.fcs(x[:, -1, :])
 
         return x
+
+class SimpleRNNModel(nn.Module):
+    def __init__(self, hidden_dim, input_dim, num_layers, num_classes):
+        super(SimpleRNNModel, self).__init__()
+
+        self.hidden_dim = hidden_dim
+        self.input_dim = input_dim
+        self.num_layers = num_layers
+
+        self.rnn = nn.RNN(input_dim, hidden_dim, num_layers, batch_first=True)
+
+        self.fcs = nn.Linear(hidden_dim, num_classes)
+
+    def forward(self, x):
+        x, _ = self.rnn(x)
+
+        x = self.fcs(x[:, -1, :])
+
+        return x
